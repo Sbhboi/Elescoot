992c7e527b8a5c961957437f831c5148f00837ac
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-25 15:50:02 +0800

    more methods but not much working haha

--- Scooter.rb
+++ Scooter.rb
@@ -1,4 +1,6 @@
 class Scooter
+    attr_reader :available, :rented, :broken
+
     def initialize(available, rented, broken)
         @available = available
         @rented = rented
@@ -15,6 +17,25 @@ class Scooter
 
     def broken
         @broken
+    end
+
+    def rent_scooter(user)
+        if @available > 0
+            @available -= 1
+            @rented += 1
+            puts "Scooter rented by #{user.name}"
+            start_time = Time.now
+            user.make_payment(5, start_time)
+            end_time = Time.now
+            calculate_rental_duration(start_time, end_time)
+        else
+            puts "No scooters available for rent."
+        end
+    end
+
+    def calculate_rental_duration(start_time, end_time)
+        duration = end_time - start_time
+        puts "Rental duration is: #{duration} minutes"
     end
 end
 
