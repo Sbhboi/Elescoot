e6920721ce1ce774570cac22c95ddb85d0d29ed7
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-26 20:25:05 +0800

    csv and txt still no input

--- User.rb
+++ User.rb
@@ -7,35 +7,40 @@ class User
         @name = name
         @contact_number = contact_number
     end
-    
-    # def name
-    #     @name
-    # end
-
-    # def contact_number
-    #     @contact_number
-    # end
 
     def make_payment(amount)
+        start_time = Time.now
         puts "Payment of #{amount} made by #{@name}."
         save_to_csv(amount, start_time)
     end
 
     def save_to_csv(amount, start_time)
+        # puts "User data saved to CSV file."
         CSV.open("user_data.csv", "a") do |csv|
-            csv << [@name, @contact_number, amount]
+            csv << [@name, @contact_number, amount, start_time]
+        end
+    end
+
+    def save_to_txt(amount, start_time)
+        File.open("user_data.txt", "a") do |file|
+            file.puts("Name #{name}")
+            file.puts("Contact Number #{contact_number}")
+            file.puts("Amount #{amount}")
+            file.puts("Start Time #{start_time}")
+            file.puts('---')
         end
     end
 end
 
-puts "Enter your name"
-user_input = gets.chomp
+puts "Welcome to Ele-Scoot"
+# puts "Enter your name"
+# user_input = gets.chomp
 
-File.open("user_data.txt", "a") do |file|
-    file.puts user_input
-end
+# File.open("user_data.txt", "a") do |file|
+#     file.puts user_input
+# end
 
-values = user_input.split(",")
-    CSV.open("user_data.csv", "a") do |csv|
-    csv << values
-end
+# values = user_input.split(",")
+#     CSV.open("user_data.csv", "a") do |csv|
+#     csv << values
+# end
